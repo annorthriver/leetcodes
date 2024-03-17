50d12b3317049f65847fd44ee44629b98336e6e4
annorthriver <annorthriver@cambricon.com>
2024-03-17 20:16:05 +0800

    [2024-3-17]feat(leetcodes): add num 9

---
+++ NUM-9.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      if (x < 0) return false;
+      vector<int> nums;
+      while(x) {
+        nums.emplace_back(x % 10);
+        x = x / 10;
+      }
+      int i = 0, j = nums.size() - 1;
+      while(i <= j) {
+        if (nums[i] != nums[j]) {
+          return false;
+        }
+        i++;
+        j--;
+      }
+      return true;
+    }
+};
