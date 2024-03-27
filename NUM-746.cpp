fea3ce9d9447c37f59d5e2b534e36dead705b97c
annorthriver <annorthriver@cambricon.com>
2024-03-27 23:09:36 +0800

    [2024-3-26]feat(leetcodes): add num 63, 746

---
+++ NUM-746.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int size = cost.size();
+        vector<int> dp(size + 1, 0);
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= size; i++) {
+            dp[i] = std::min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[size];
+    }
+};
